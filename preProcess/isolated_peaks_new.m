b5d0f671006658bd81343672cfb0395ba0b86098
Mike Manookin <manookin@uw.edu>
2025-06-21 20:08:31 -0700

    Mod of isolated_peaks to see if this fixes lost spikes.

--- preProcess/isolated_peaks_new.m
+++ preProcess/isolated_peaks_new.m
@@ -12,14 +12,14 @@ nt0 = ops.nt0;
 
 % finding the local minimum in a sliding window within plus/minus loc_range extent
 % across time and across channels
-smin = my_min(S1, loc_range, [1 2]); % This should be a logical matrix.
+% smin = my_min(S1, loc_range, [1 2]); % This should be a logical matrix.
 % peaks = single(S1<smin+1e-3 & S1<Th); % the peaks are samples that achieve this local minimum, AND have negativities less than a preset threshold
 
 % Logical matrix of detected spikes
-peaks = (S1<smin+1e-3 & S1<Th);
+% peaks = (S1<smin+1e-3 & S1<Th);
 
-% local_min = mike_min(S1);
-% peaks = local_min & (S1 < Th*100);
+local_min = mike_min(S1);
+peaks = local_min & (S1 < Th*100);
 
 % only take local peaks that are isolated from other local peaks
 % sum_peaks = my_sum(peaks, long_range, [1 2]); % if there is another local peak close by, this sum will be at least 2
