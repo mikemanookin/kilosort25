62cc74370cf5bb9420f069f47c140ffce6ea85e4
Mike Manookin <manookin@uw.edu>
2025-07-09 14:53:25 -0700

    Updates to spike sorting pipeline.

--- preProcess/isolated_peaks_new.m
+++ preProcess/isolated_peaks_new.m
@@ -12,18 +12,23 @@ nt0 = ops.nt0;
 
 % finding the local minimum in a sliding window within plus/minus loc_range extent
 % across time and across channels
-smin = my_min(S1, loc_range, [1 2]); % This should be a logical matrix.
-% peaks = single(S1<smin+1e-3 & S1<Th); % the peaks are samples that achieve this local minimum, AND have negativities less than a preset threshold
+if sum(loc_range) > 2
+    smin = my_min(S1, loc_range, [1 2]); % This should be a logical matrix.
+    peaks = (S1<smin+1e-3 & S1<Th); % the peaks are samples that achieve this local minimum, AND have negativities less than a preset threshold
+else % Local minimum calculation.
+    local_min = find_local_minima(S1);
+    peaks = local_min & (S1 < Th);
+end
 
 % Logical matrix of detected spikes
-peaks = (S1<smin+1e-3 & S1<Th);
-
-% local_min = mike_min(S1);
-% peaks = local_min & (S1 < Th);
+% peaks = (S1<smin+1e-3 & S1<Th);
 
 % only take local peaks that are isolated from other local peaks
-% sum_peaks = my_sum(peaks, long_range, [1 2]); % if there is another local peak close by, this sum will be at least 2
-% peaks = peaks .* (sum_peaks<1.2) .* S1; % set to 0 peaks that are not isolated, and multiply with the voltage values
+if sum(long_range) > 2 && sum(loc_range) > 2
+    sum_peaks = my_sum(peaks, long_range, [1 2]); % if there is another local peak close by, this sum will be at least 2
+    % peaks = single(peaks) .* (sum_peaks<1.2) .* S1; % set to 0 peaks that are not isolated, and multiply with the voltage values
+    peaks = peaks & (sum_peaks<1.2);
+end
 
 % exclude temporal buffers
 peaks([1:nt0 end-nt0:end], :) = 0;
